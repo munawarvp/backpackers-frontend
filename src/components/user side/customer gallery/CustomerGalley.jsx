c5e2f082b1ec0ed531b3049dec761b94c47f863d
munawarvp <vpmunawar@gmail.com>
2023-05-15 20:13:44 +0530

    review img fixed

--- src/components/user side/customer gallery/CustomerGalley.jsx
+++ src/components/user side/customer gallery/CustomerGalley.jsx
@@ -45,19 +45,19 @@ function CustomerGalley() {
                         </div>
                         <div className="gallery-img-first-row">
                             <div className="gallery-img-first-row-contain">
-                                <img className='gallery-img' src={`${review_two.review_image}`} alt="" />
+                                {review_two && <img className='gallery-img' src={`${review_two.review_image}`} alt="" />}
                             </div>
                             <div className="gallery-img-first-row-contain">
-                                <img className='gallery-img' src={`${review_three.review_image}`} alt="" />
+                                {review_three && <img className='gallery-img' src={`${review_three.review_image}`} alt="" />}
                             </div>
                         </div>
                     </div>
                     <div className="galley-first-row">
                         <div className="gallery-second-container">
-                            <img className='gallery-img' src={`${review_four.review_image}`} alt="" />
+                            {review_four && <img className='gallery-img' src={`${review_four.review_image}`} alt="" />}
                         </div>
                         <div className="gallery-third-container">
-                            <img className='gallery-img' src={`${review_five.review_image}`} alt="" />
+                            {review_five && <img className='gallery-img' src={`${review_five.review_image}`} alt="" />}
                         </div>
                     </div>
 
