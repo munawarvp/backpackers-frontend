96a0f155e77ce6887322560dcc1190a15bf71041
munawarvp <vpmunawar@gmail.com>
2023-05-15 19:15:34 +0530

    base url set

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:8000'
+export const BASE_URL = 'https://api.backpackers.cloud'
 
 export const mapbox_access_token = 'pk.eyJ1IjoibGFzZXJsYWQiLCJhIjoiY2xnaXoweTBpMTBuZTNkcGh6YjJ6emh6aiJ9.fzEafvzlOKjLZPcreOKjDg'
